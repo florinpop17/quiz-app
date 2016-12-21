ad9520ce403a30e2a0f0244f7f6876400dc2edd7
Florin Pop <popflorin1705@yahoo.com>
2016-12-22 01:31:53 +0200

    next question functionality

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,10 +5,42 @@ class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            question: 1
+            nr: 0,
+            total: data.length
         }
+        this.nextQuestion = this.nextQuestion.bind(this);
     }
+    
+    pushData(nr) {        
+        this.setState({
+            question: data[nr].question,
+            answerA: data[nr].answers[0],
+            answerB: data[nr].answers[1],
+            answerC: data[nr].answers[2],
+            answerD: data[nr].answers[3],
+            nr: this.state.nr + 1
+        });
+    }
+    
+    componentWillMount() {
+        let { nr } = this.state
+        this.pushData(nr);
+    }
+    
+    nextQuestion() {
+        let { nr, total } = this.state;
+        
+        if(nr === total){
+            alert("You finished the quiz!");
+        } else {
+            this.pushData(nr);
+        }
+        
+    }
+    
     render() {
+        let { nr, total, question, answerA, answerB, answerC, answerD } = this.state;
+        
         return (
             <div className="container">
                 <div className="row">
@@ -16,19 +48,19 @@ class Main extends React.Component {
                         <h1>Quiz application</h1>
                 
                         <div id="question">
-                            <h4>Question 1/10</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima recusandae culpa commodi tempore, qui voluptatibus nisi atque maiores error.</p>
+                            <h4>Question {nr}/{total}</h4>
+                            <p>{question}</p>
                         </div>
                         <div id="answers">
                             <ul>
-                                <li><span>A</span> <p>Long answer A Long answer A Long answer A</p></li>
-                                <li><span>B</span> <p>Answer B</p></li>
-                                <li><span>C</span> <p>Long answer C Long answer C Long answer C Long answer C Long answer C Long answer C</p></li>
-                                <li><span>D</span> <p>Answer D</p></li>
+                                <li><span>A</span> <p>{answerA}</p></li>
+                                <li><span>B</span> <p>{answerB}</p></li>
+                                <li><span>C</span> <p>{answerC}</p></li>
+                                <li><span>D</span> <p>{answerD}</p></li>
                             </ul>
                         </div>
                         <div id="submit">
-                            <button>Submit</button>
+                            <button onClick={this.nextQuestion}>Submit</button>
                         </div>
                         
                     </div>
--- app/data/data.js
+++ app/data/data.js
@@ -1,25 +1,23 @@
-export default { 
-    data: [
-        {
-            question: 'Question 1',
-            answers: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
-            correct: 4
-        },
+export default [
+    {
+        question: 'Question 1',
+        answers: ['Answer 11', 'Answer 12', 'Answer 13', 'Answer 14'],
+        correct: 4
+    },
 
-        {
-            question: 'Question 2',
-            answers: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
-            correct: 1
-        },    
-        {
-            question: 'Question 3',
-            answers: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
-            correct: 2
-        },    
-        {
-            question: 'Question 4',
-            answers: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
-            correct: 3
-        }
-    ]
-}
+    {
+        question: 'Question 2',
+        answers: ['Answer 21', 'Answer 22', 'Answer 23', 'Answer 24'],
+        correct: 1
+    },    
+    {
+        question: 'Question 3',
+        answers: ['Answer 31', 'Answer 32', 'Answer 33', 'Answer 34'],
+        correct: 2
+    },    
+    {
+        question: 'Question 4',
+        answers: ['Answer 41', 'Answer 42', 'Answer 43', 'Answer 44'],
+        correct: 3
+    }
+]
