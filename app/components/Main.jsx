66e7e23ff3b59fe1fa0070bb9480340b57096eba
Florin Pop <popflorin1705@yahoo.com>
2016-12-21 22:56:59 +0200

    basic html structure

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -10,6 +10,22 @@ class Main extends React.Component {
         return (
             <div>
                 <h1>{this.state.title}</h1>
+                
+                <div id="question">
+                    <h2>Question 1/10</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima recusandae culpa commodi tempore, qui voluptatibus nisi atque maiores error, quod omnis, autem ipsum quaerat impedit nesciunt id a, excepturi. Hic.</p>
+                </div>
+                <div id="answers">
+                    <ul>
+                        <li>Answer A</li>
+                        <li>Answer B</li>
+                        <li>Answer C</li>
+                        <li>Answer D</li>
+                    </ul>
+                </div>
+                <div id="submit">
+                    <button>Submit</button>
+                </div>
             </div>
         );
     }
