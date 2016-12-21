c300ed288952e32bc2fa3be705f9a4125bf828ac
Florin Pop <popflorin1705@yahoo.com>
2016-12-21 23:09:09 +0200

    bootstrap container>row>grid + styling header

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -8,23 +8,28 @@ class Main extends React.Component {
     
     render() {
         return (
-            <div>
-                <h1>{this.state.title}</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-10 col-md-offset-1">
+                        <h1>{this.state.title}</h1>
                 
-                <div id="question">
-                    <h2>Question 1/10</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima recusandae culpa commodi tempore, qui voluptatibus nisi atque maiores error, quod omnis, autem ipsum quaerat impedit nesciunt id a, excepturi. Hic.</p>
-                </div>
-                <div id="answers">
-                    <ul>
-                        <li><span>A</span>Answer A</li>
-                        <li><span>B</span>Answer B</li>
-                        <li><span>C</span>Answer C</li>
-                        <li><span>D</span>Answer D</li>
-                    </ul>
-                </div>
-                <div id="submit">
-                    <button>Submit</button>
+                        <div id="question">
+                            <h4>Question 1/10</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima recusandae culpa commodi tempore, qui voluptatibus nisi atque maiores error, quod omnis, autem ipsum quaerat impedit nesciunt id a, excepturi. Hic.</p>
+                        </div>
+                        <div id="answers">
+                            <ul>
+                                <li><span>A</span>Answer A</li>
+                                <li><span>B</span>Answer B</li>
+                                <li><span>C</span>Answer C</li>
+                                <li><span>D</span>Answer D</li>
+                            </ul>
+                        </div>
+                        <div id="submit">
+                            <button>Submit</button>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         );
