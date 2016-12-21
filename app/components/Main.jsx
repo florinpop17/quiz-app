f5938687b8b6fbbbbc0d1239251e0cf829552f21
Florin Pop <popflorin1705@yahoo.com>
2016-12-21 22:54:31 +0200

    es6 code

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 class Main extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {title: 'Quiz app'}
     }
     
     render() {
         return (
             <div>
-                <h1>Quiz app</h1>
+                <h1>{this.state.title}</h1>
             </div>
         );
     }
