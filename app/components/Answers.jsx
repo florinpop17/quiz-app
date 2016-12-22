de37808e6d71c90db1484a2b22df59978d3573a1
Florin Pop <popflorin1705@yahoo.com>
2016-12-22 10:53:20 +0200

    handleClick event

--- app/components/Answers.jsx
+++ app/components/Answers.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 class Answers extends React.Component {
     constructor(props) {
         super(props);
     }
     
-    handleCheck() {
-        alert('handleCheck');
+    handleCheck(e) {
+        console.log(e);
     }
     
     render() {
         return (
             <div id="answers">
                 <ul>
-                    <li onClick={this.handleCheck}><span>A</span> <p>{props.answers[0]}</p></li>
-                    <li onClick={this.handleCheck}><span>B</span> <p>{props.answers[1]}</p></li>
-                    <li onClick={this.handleCheck}><span>C</span> <p>{props.answers[2]}</p></li>
-                    <li onClick={this.handleCheck}><span>D</span> <p>{props.answers[3]}</p></li>
+                    <li onClick={this.handleCheck}><span>A</span> <p>{this.props.answers[0]}</p></li>
+                    <li onClick={this.handleCheck}><span>B</span> <p>{this.props.answers[1]}</p></li>
+                    <li onClick={this.handleCheck}><span>C</span> <p>{this.props.answers[2]}</p></li>
+                    <li onClick={this.handleCheck}><span>D</span> <p>{this.props.answers[3]}</p></li>
                 </ul>
             </div>
         );
