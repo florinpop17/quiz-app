7d32a89a7a2be0779f91be148edc3ac245b2d050
Florin Pop <popflorin1705@yahoo.com>
2016-12-22 11:53:10 +0200

    data modif

--- app/data/data.js
+++ app/data/data.js
@@ -1,23 +1,23 @@
 export default [
     {
         question: 'Question 1',
-        answers: ['Answer 11', 'Answer 12', 'Answer 13', 'Answer 14'],
+        answers: ['Answer 11', 'Answer 12', 'Answer 13', 'AnswerC 14'],
         correct: 4
     },
 
     {
         question: 'Question 2',
-        answers: ['Answer 21', 'Answer 22', 'Answer 23', 'Answer 24'],
+        answers: ['AnswerC 21', 'Answer 22', 'Answer 23', 'Answer 24'],
         correct: 1
     },    
     {
         question: 'Question 3',
-        answers: ['Answer 31', 'Answer 32', 'Answer 33', 'Answer 34'],
+        answers: ['Answer 31', 'AnswerC 32', 'Answer 33', 'Answer 34'],
         correct: 2
     },    
     {
         question: 'Question 4',
-        answers: ['Answer 41', 'Answer 42', 'Answer 43', 'Answer 44'],
+        answers: ['Answer 41', 'Answer 42', 'AnswerC 43', 'Answer 44'],
         correct: 3
     }
 ]
