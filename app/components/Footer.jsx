3e135c828e0afe37483c885c7d89e91b6ee84be1
Florin Pop <popflorin1705@yahoo.com>
2016-12-23 11:40:41 +0200

    added footer styling

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <footer>
+                <p>Built with &hearts; by <a href="http://florin-pop.com">Florin Pop</a></p>
+            </footer>
+        )
+    }
+}
+
+export default Footer
