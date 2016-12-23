ce067cd95376e3c52b667cd5f05da7fc631a1b43
Florin Pop <popflorin1705@yahoo.com>
2016-12-23 13:01:34 +0200

    moved bind to constructor

--- app/components/Answers.jsx
+++ app/components/Answers.jsx
@@ -8,6 +8,8 @@ class Answers extends React.Component {
             classNames: ['', '', '', ''],
             score: 0
         }
+        
+        this.checkAnswer = this.checkAnswer.bind(this);
     }
     
     checkAnswer(e) {
@@ -40,7 +42,7 @@ class Answers extends React.Component {
     shouldComponentUpdate() {
         this.setState({
             classNames: ['', '', '', '']
-        })
+        });
         return true;
     }
     
@@ -51,10 +53,10 @@ class Answers extends React.Component {
         return (
             <div id="answers">
                 <ul>
-                    <li onClick={this.checkAnswer.bind(this)} className={classNames[0]} data-id="1"><span>A</span> <p>{answers[0]}</p></li>
-                    <li onClick={this.checkAnswer.bind(this)} className={classNames[1]} data-id="2"><span>B</span> <p>{answers[1]}</p></li>
-                    <li onClick={this.checkAnswer.bind(this)} className={classNames[2]} data-id="3"><span>C</span> <p>{answers[2]}</p></li>
-                    <li onClick={this.checkAnswer.bind(this)} className={classNames[3]} data-id="4"><span>D</span> <p>{answers[3]}</p></li>
+                    <li onClick={this.checkAnswer} className={classNames[0]} data-id="1"><span>A</span> <p>{answers[0]}</p></li>
+                    <li onClick={this.checkAnswer} className={classNames[1]} data-id="2"><span>B</span> <p>{answers[1]}</p></li>
+                    <li onClick={this.checkAnswer} className={classNames[2]} data-id="3"><span>C</span> <p>{answers[2]}</p></li>
+                    <li onClick={this.checkAnswer} className={classNames[3]} data-id="4"><span>D</span> <p>{answers[3]}</p></li>
                 </ul>
             </div>
         );
