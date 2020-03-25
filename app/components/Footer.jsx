935d472ed849bc1ff85a9219dbbb2c2d5ce44ec3
Jill Platts <jillplatts@gmail.com>
2020-03-25 00:05:38 -0400

    Update Footer.jsx
    Application spelling. 😃

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ class Footer extends React.Component{
         return (
             <footer>
                 <small>
-                    You can find the code repo on <a href="https://github.com/Flopet17/quiz-app">Github</a>. Feel free to make any pull requests to improve the aplication.
+                    You can find the code repo on <a href="https://github.com/Flopet17/quiz-app">Github</a>. Feel free to make any pull requests to improve the application.
                 </small>
                 <p>Built with <span>&hearts;</span> by <a href="http://florin-pop.com">Florin Pop</a></p>
             </footer>
